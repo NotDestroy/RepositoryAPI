f9c24229ec406eadaa162a38516ad7a6725614bc
NotDestroy <t7489245>
2018-11-20 18:51:13 +0200

    api

---
+++ src/UserApi.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Api\Base;
+
+class UserApi extends Api
+{
+    private $apiName = 'users';
+
+    public function indexAction()
+    {
+
+    }
+
+    public function viewAction()
+    {
+
+    }
+
+    public function createAction()
+    {
+
+    }
+
+    public function updateAction()
+    {
+
+    }
+
+    public function deleteAction()
+    {
+
+    }
+
+}
