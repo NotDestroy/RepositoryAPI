ae0dbcf460a65339d8cda1c42c77fc2df725b020
NotDestroy <t7489245>
2018-11-22 15:21:09 +0200

    fourth correction application

--- src/Application.php
+++ src/Application.php
@@ -6,6 +6,7 @@ class Application
 {
     private $requestUri;
     private $method;
+    public  $processResult = [];
 
     // ########################################
 
@@ -13,11 +14,12 @@ class Application
     {
         $this->requestUri = $_SERVER['REQUEST_URI'];
         $this->method     = $_SERVER['REQUEST_METHOD'];
+        $this->process();
     }
 
     // ########################################
 
-    public function process()
+    private function process()
     {
 
         $obResurs = \Api\Base\Resources::getInstanceResurs();
@@ -35,12 +37,11 @@ class Application
             if (!array_key_exists('handler', $oneCommand)) {
                 throw new \Api\Base\Config\Exception\KeyNotFound('Ошибка: такого ключа не существует');
             }
-            if (is_subclass_of($oneCommand['handler'],
-                    'Api\Base\Command\Handler\BaseInterface') && $oneCommand['method'] === $this->method
-                && $oneCommand['path'] === $this->requestUri) {
-                $ob = new $oneCommand['handler']();
-
-                return $ob->process();
+            if ($oneCommand['method'] === $this->method
+                && $oneCommand['path'] === $this->requestUri && is_subclass_of($oneCommand['handler'],
+                    'Api\Base\Command\Handler\BaseInterface')) {
+                $obHandler  = new $oneCommand['handler']();
+                $this->processResult = $obHandler->process();
             }
         }
 
